301c2cf4639a79c361743487080442450744e8e4
Anthony Romagnolo <anthonyroma741@gmail.com>
2023-06-04 14:33:52 -0400

    fixed mainpage css -used flexboxes and grid -change button css, add colors to root as variable. -work on heading next

--- src/components/main_page/MainPage.jsx
+++ src/components/main_page/MainPage.jsx
@@ -1,36 +1,46 @@
-import React, { useState } from 'react';
+import React, { useState } from 'react'
 import './mainpage.css'
 
 const MainPage = () => {
-    const [amount, setAmount] = useState('');
-    const [accountBalance, setAccountBalance] = useState(0);
-    const [depositedAmount, setDepositedAmount] = useState(0);
-    const [currentProtocol, setCurrentProtocol] = useState('');
-  
-    const handleAmountChange = (e) => {
-      setAmount(e.target.value);
-    }
-  
-    // Add handlers for Deposit, Rebalance, and Withdraw here
-  
-    return (
-      <div>
-        <h1>Yield Aggregator</h1>
-        <div className="row">
-          <div className="column">
-            <input type="text" value={amount} onChange={handleAmountChange} placeholder="Enter amount to deposit" />
-            <button>Deposit</button>
-            <button>Rebalance</button>
-            <button>Withdraw</button>
+  const [amount, setAmount] = useState('')
+  const [accountBalance, setAccountBalance] = useState(0)
+  const [depositedAmount, setDepositedAmount] = useState(0)
+  const [currentProtocol, setCurrentProtocol] = useState('')
+
+  const handleAmountChange = (e) => {
+    setAmount(e.target.value)
+  }
+
+  // Add handlers for Deposit, Rebalance, and Withdraw here
+
+  return (
+    <div>
+      <h1>Yield Aggregator</h1>
+      <div className="main-container">
+        <div className="main-row">
+          <div className="main-column-left">
+            <input
+              type="text"
+              value={amount}
+              onChange={handleAmountChange}
+              placeholder="Enter amount to deposit"
+            />
+            <button className="main-button">Deposit</button>
+            <button className="main-button">Rebalance</button>
+            <button className="main-button">Withdraw</button>
           </div>
-          <div className="column">
+          <div className="main-column-right">
             <p>Current balance: {accountBalance}</p>
-            <p>Amount deposited to the aggregator smart contract: {depositedAmount}</p>
+            <p>
+              Amount deposited to the aggregator smart contract:{' '}
+              {depositedAmount}
+            </p>
             <p>Current protocol where funds are deposited: {currentProtocol}</p>
           </div>
         </div>
       </div>
-    );
-  };
-  
-  export default MainPage;
+    </div>
+  )
+}
+
+export default MainPage
